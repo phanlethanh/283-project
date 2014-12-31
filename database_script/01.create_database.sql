d1fb5848768b9e01f58dce76eeab33a8a4108e01
phanlethanh93 <phanlethanh93@gmail.com>
2014-12-31 14:39:37 +0000

    Add stock_quantity field into product table. Update others

--- database_script/01.create_database.sql
+++ database_script/01.create_database.sql
@@ -4,7 +4,7 @@
 
 CREATE DATABASE onlinestore
   WITH OWNER = postgres
-       ENCODING = 'unicode'
+       ENCODING = 'UTF8'
        TABLESPACE = pg_default
        LC_COLLATE = 'English_United States.1252'
        LC_CTYPE = 'English_United States.1252'
